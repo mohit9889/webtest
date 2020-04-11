4da83c1dd3eeaf4f6dd8d423b11181fdd48b305f
Mohit Rajput <raj.mohit9889@gmail.com>
2020-04-11 23:40:24 +0530

    new page

---
+++ web-grid-anim/js/ainm.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // wait until window is loaded
+    window.addEventListener("load", function (e) {
+
+        document.body.style.display = 'block';
+        var tl = new TimelineMax();
+
+        tl.staggerFrom('section', 2, {
+            opacity: 0,
+            scale: .5,
+            ease: Power2.easeOut
+        }, 0.2)
+
+        tl.staggerFrom('h1, h2', .5, {
+            opacity: 0,
+            y: -40,
+            ease: Power2.easeInOut
+        }, 0.2, "-=2")
+
+        tl.staggerFrom('.para', 1, {
+            opacity: 0,
+            y: -40,
+            ease: Power2.easeInOut
+        }, 0.2, "-=1.5")
+
+    }, false);
+});
