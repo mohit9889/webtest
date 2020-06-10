801afc1fcf9ba41b4b52a8e0853bca278f0cc415
mohit9889 <raj.mohit9889@gmail.com>
2020-06-10 16:01:13 +0530

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,19 @@ navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
 
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
